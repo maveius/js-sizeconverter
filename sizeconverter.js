1488abcece610d9998f4948093740d10ad4ee54a
Michael Ligus <maveilthain@gmail.com>
2015-02-14 16:02:51 +0100

    Initial commit with push main file to GitHub

---
+++ sizeconverter.js
@@ -0,0 +1,37 @@
+function SizeConverter () {
+
+    this.multiplayer = 1024;
+    this.sizes = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    this.convert = function (value, sourceUnit, destinationUnit) {
+
+        if (sourceUnit === undefined &&
+            destinationUnit === undefined ||
+            sourceUnit == destinationUnit) {
+
+            return value;
+        }
+
+        var sourceIndex = ( this.sizes.indexOf(sourceUnit) );
+        var destinationIndex = ( this.sizes.indexOf(destinationUnit) );
+
+        var result = value;
+        var exponent = 0;
+        if ( sourceIndex > destinationIndex ) {
+
+            exponent = sourceIndex - destinationIndex;
+            result = value * Math.pow(this.multiplayer, exponent);
+            return parseFloat(result);
+
+        } else if ( sourceIndex < destinationIndex ) {
+
+            exponent = destinationIndex - sourceIndex;
+            result = value / Math.pow(this.multiplayer, exponent);
+            return parseFloat(result);
+
+        } else {
+
+            return result;
+        }
+    };
+}
